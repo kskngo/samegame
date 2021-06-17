5786f9c28ed6b3ca0e510841d26dacd4048cb3a0
Kosuke Nagao <kosukengo@gmail.com>
2021-06-18 02:18:06 +0900

    タッチしたカードを消せる処理

--- samegame/ViewController.swift
+++ samegame/ViewController.swift
@@ -63,48 +63,76 @@ class ViewController: UIViewController {
         print(touches.first!.location(in: view))
 
         let position = touches.first!.location(in: view)
+        let selectedColumn: Int?
+        let selectedRow: Int?
 
         switch position.x {
         case cards[0][0].frame.minX...cards[0][0].frame.maxX:
             print("col1")
+            selectedColumn = 0
         case cards[0][1].frame.minX...cards[0][1].frame.maxX:
             print("col2")
+            selectedColumn = 1
         case cards[0][2].frame.minX...cards[0][2].frame.maxX:
             print("col3")
+            selectedColumn = 2
         case cards[0][3].frame.minX...cards[0][3].frame.maxX:
             print("col4")
+            selectedColumn = 3
         case cards[0][4].frame.minX...cards[0][4].frame.maxX:
             print("col5")
+            selectedColumn = 4
         case cards[0][5].frame.minX...cards[0][5].frame.maxX:
             print("col6")
+            selectedColumn = 5
         case cards[0][6].frame.minX...cards[0][6].frame.maxX:
             print("col7")
+            selectedColumn = 6
         case cards[0][7].frame.minX...cards[0][7].frame.maxX:
             print("col8")
+            selectedColumn = 6
         default:
-            print("none")
+            selectedColumn = nil
         }
 
         switch position.y {
         case cards[0][0].frame.minY...cards[0][0].frame.maxY:
             print("r1")
+            selectedRow = 0
         case cards[1][0].frame.minY...cards[1][0].frame.maxY:
             print("r2")
+            selectedRow = 1
         case cards[2][0].frame.minY...cards[2][0].frame.maxY:
             print("r3")
+            selectedRow = 2
         case cards[3][0].frame.minY...cards[3][0].frame.maxY:
             print("r4")
+            selectedRow = 3
         case cards[4][0].frame.minY...cards[4][0].frame.maxY:
             print("r5")
+            selectedRow = 4
         case cards[5][0].frame.minY...cards[5][0].frame.maxY:
             print("r6")
+            selectedRow = 5
         case cards[6][0].frame.minY...cards[6][0].frame.maxY:
             print("r7")
+            selectedRow = 6
         case cards[7][0].frame.minY...cards[7][0].frame.maxY:
             print("r8")
+            selectedRow = 7
         default:
             print("none")
+            selectedRow = nil
         }
+
+        // 選択されたセルに対する処理
+        if let selectedColumn = selectedColumn, let selectedRow = selectedRow {
+//            cards[selectedColumn][selectedRow].backgroundColor = .clear
+//            cards[selectedColumn][selectedRow].textColor = .clear
+            cards[selectedRow][selectedColumn].isHidden = true
+        }
+
+
         //
 
         //
