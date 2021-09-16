98992f10cb8fcad10408736fa288e9c4687d733b
kskngo <kosukengo@gmail.com>
2021-09-16 19:39:39 +0900

    空白を縦に詰める処理を追加

--- samegame.playground/Contents.swift
+++ samegame.playground/Contents.swift
@@ -2,26 +2,53 @@ import UIKit
 
 struct Card<CardContent> {
     var value: CardContent
-    var rowIndex: Int
-    var columIndex: Int
     var isFixed: Bool = false
 }
 
-struct CardsState {
+
+struct Score {
+    private (set) var totalValue: Int
+    var value: Int {
+        didSet {
+            totalValue +=  value
+        }
+    }
+}
+enum GameState {
+    case newGame
+    case notSelected
+    case selecded
+    case gameOver
+    case clear
+}
+
+func updateScore() {
+    score.value += 1
+}
+
+class Game {
+
+    init() {
+    }
+
     static let numberOfRows =  5
     static let numberOfColumns = 5
-//    let cardValues = ["👻", "🐤"]
-    let cardValues = ["👻"]
-//    let cardValues = ["👻", "🐤", "🍄"]
+//    var score: Score
+
+    let cardValues = ["👻", "🐤"]
+    //    let cardValues = ["👻"]
+    //    let cardValues = ["👻", "🐤", "🍄"]
 
     var cards = [[Card<String>]]()
 
-    mutating func reset() {
-        for _ in 0..<CardsState.numberOfRows {
+
+
+    func reset() {
+        for _ in 0..<Game.numberOfRows {
             var row:[Card<String>] = []
-            for j in 0..<CardsState.numberOfColumns {
+            for _ in 0..<Game.numberOfColumns {
                 let value = cardValues.randomElement()!
-                let card = Card(value: value, rowIndex: 0, columIndex: j)
+                let card = Card(value: value)
                 row.append(card)
             }
             cards.append(row)
@@ -43,118 +70,202 @@ struct CardsState {
         print("---------")
     }
 
+
+    func startNewGame() {
+        reset()
+//        cards.printCards()
+    }
+
+    // カードがタップされたときの処理
+    func tapedCard(rowIndex: Int, coloumIndex: Int) {
+        print("tapped at \(rowIndex):\(coloumIndex)")
+        printCards()
+        updateCards(rowIndex: rowIndex, coloumIndex: coloumIndex)
+        updateScore()
+        printCards()
+    }
+
+    func updateCards(rowIndex: Int, coloumIndex: Int) {
+        let selectedCardValue = cards[rowIndex][coloumIndex].value
+        // カードを消す
+        deleteCards(selectedCardValue: selectedCardValue, rowIndex: rowIndex, coloumIndex: coloumIndex)
+//        printCards()
+        // カードを移動
+        moveCards()
+    }
+
+    func deleteCards(selectedCardValue: String, rowIndex: Int, coloumIndex: Int) {
+        // タップされたカードの上下左右を消す
+        deleteCardsOnFourDirections(selectedCardValue: selectedCardValue, rowIndex: rowIndex, coloumIndex: coloumIndex)
+
+    }
+    // 対象となるカードの上下左右にあるカードを消す
+    func deleteCardsOnFourDirections(selectedCardValue: String, rowIndex: Int, coloumIndex: Int) {
+
+        //上にあるカードを消す
+        let isFixedCardTop = deleteCard(selectedCardValue: selectedCardValue, targetRowIndex: rowIndex - 1, targetColoumIndex: coloumIndex)
+        // 下にあるカードを消す
+        let isFixedCardBottom = deleteCard(selectedCardValue: selectedCardValue, targetRowIndex: rowIndex + 1, targetColoumIndex: coloumIndex)
+        // 左にあるカードを消す
+        let isFixedCardLeft = deleteCard(selectedCardValue: selectedCardValue, targetRowIndex: rowIndex, targetColoumIndex: coloumIndex - 1)
+        // 右にあるカードを消す
+        let isFixedCardRight = deleteCard(selectedCardValue: selectedCardValue, targetRowIndex: rowIndex, targetColoumIndex: coloumIndex + 1)
+
+        // １つでも消したカードがあれば対象カードも消す
+        if isFixedCardTop || isFixedCardBottom || isFixedCardLeft || isFixedCardRight {
+            cards[rowIndex][coloumIndex].isFixed = true
+        }
+
+        if isFixedCardTop {
+            // 対象を上に移動して再起呼び出し
+            deleteCardsOnFourDirections(selectedCardValue: selectedCardValue, rowIndex: rowIndex - 1, coloumIndex: coloumIndex)
+        }
+
+        if isFixedCardBottom {
+            // 対象を下に移動して再起呼び出し
+            deleteCardsOnFourDirections(selectedCardValue: selectedCardValue, rowIndex: rowIndex + 1, coloumIndex: coloumIndex)
+        }
+
+        if isFixedCardLeft {
+            // 対象を左に移動して再起呼び出し
+            deleteCardsOnFourDirections(selectedCardValue: selectedCardValue, rowIndex: rowIndex, coloumIndex: coloumIndex - 1)
+        }
+
+        if isFixedCardRight {
+            // 対象を右に移動して再起呼び出し
+            deleteCardsOnFourDirections(selectedCardValue: selectedCardValue, rowIndex: rowIndex, coloumIndex: coloumIndex + 1)
+        }
+    }
+
+    func deleteCard(selectedCardValue: String, targetRowIndex: Int, targetColoumIndex: Int) -> Bool {
+
+        if existsIndex(targetRowIndex: targetRowIndex, targetColoumIndex: targetColoumIndex) {
+
+            if !cards[targetRowIndex][targetColoumIndex].isFixed {
+                if isMatchCard(selectedCardValue: selectedCardValue, targetRowIndex: targetRowIndex, targetColoumIndex: targetColoumIndex) {
+                    cards[targetRowIndex][targetColoumIndex].isFixed = true
+                    return true
+                }
+            }
+        }
+        return false
+    }
+
+    // カードがselectedCardValueと一致する場合trueを返す。一致しない場合falseを返す。
+    func isMatchCard(selectedCardValue: String, targetRowIndex: Int, targetColoumIndex: Int) -> Bool {
+
+        // カードインデックスが存在するか確認
+        guard existsIndex(targetRowIndex: targetRowIndex, targetColoumIndex: targetColoumIndex) else {
+            fatalError("存在しないインデックス指定されています")
+        }
+        let targetCardValue = cards[targetRowIndex][targetColoumIndex].value
+        return selectedCardValue == targetCardValue
+    }
+
     func existsIndex(targetRowIndex: Int, targetColoumIndex: Int) -> Bool {
-        0 <= targetColoumIndex && targetColoumIndex < Self.numberOfColumns
-            && 0 <= targetRowIndex && targetRowIndex < Self.numberOfRows
+        0 <= targetColoumIndex && targetColoumIndex < Game.numberOfColumns
+            && 0 <= targetRowIndex && targetRowIndex < Game.numberOfRows
     }
-}
 
-struct Score {
+    func moveCards()  {
+        moveCardsFromTopToBottom()
+        moveCardsFromRightToLeft()
+    }
 
-    private (set) var totalValue: Int
-    var value: Int {
-        didSet {
-            totalValue +=  value
+    func moveCardsFromTopToBottom() {
+        for columnIndex in 0..<Game.numberOfColumns {
+            moveOneColoumOfCardsFromTopToBottom(columnIndex: columnIndex)
         }
     }
-}
-enum GameState {
-    case newGame
-    case notSelected
-    case selecded
-    case gameOver
-    case clear
-}
 
-func tapedCard(rowIndex: Int, coloumIndex: Int) {
-    updateCards(rowIndex: rowIndex, coloumIndex: coloumIndex)
-    updateScore()
-}
+    // １列に対する移動処理
+    func moveOneColoumOfCardsFromTopToBottom(columnIndex: Int) {
 
-func updateCards(rowIndex: Int, coloumIndex: Int) {
-    let selectedCardValue = cardsState.cards[rowIndex][coloumIndex].value
+        for rowIndex in (0..<Game.numberOfRows).reversed() {
 
-    // タップされたカードの右側にあるカードを消す
-    deleteMatchCards(selectedCardValue, rowIndex, coloumIndex, getTargetRowIndex: { $0 }, getTargetColoumIndex: { $0 + 1 })
-    // タップされたカードの左側にあるカードを消す
-    deleteMatchCards(selectedCardValue, rowIndex, coloumIndex, getTargetRowIndex: { $0 }, getTargetColoumIndex: { $0 - 1 })
-    // タップされたカードの下側にあるカードを消す
-    deleteMatchCards(selectedCardValue, rowIndex, coloumIndex, getTargetRowIndex: { $0 + 1 }, getTargetColoumIndex: { $0 })
-    cardsState.printCards()
-}
-
-// 隣り合う同じカードを消す
-func deleteMatchCards(_ selectedCardValue: String, _ rowIndex: Int, _ coloumIndex: Int,
-                      getTargetRowIndex: (Int) -> Int, getTargetColoumIndex: (Int) -> Int) {
-    if let isMatch = isMatchCard(selectedCardValue: selectedCardValue,
-                                 rowIndex: rowIndex, coloumIndex: coloumIndex,
-                                 getTargetRowIndex: getTargetRowIndex,
-                                 getTargetColoumIndex: getTargetColoumIndex) {
-
-        // 隣のカードが同じ場合
-        if isMatch {
-            cardsState.cards[rowIndex][coloumIndex].isFixed = true
-            cardsState.cards[rowIndex][getTargetColoumIndex(coloumIndex)].isFixed = true
-            // 隣のカードに対して再起呼び出し
-            deleteMatchCards(selectedCardValue,
-                             getTargetRowIndex(rowIndex),
-                             getTargetColoumIndex(coloumIndex),
-                             getTargetRowIndex: getTargetRowIndex,
-                             getTargetColoumIndex: getTargetColoumIndex)
+            if existsIndex(targetRowIndex: rowIndex, targetColoumIndex: columnIndex) &&
+                cards[rowIndex][columnIndex].value == "" {
+                let isMoved = moveCardsFromTopToBottom(rowIndex: rowIndex ,columnIndex: columnIndex)
+                if isMoved && existsTargetCard(targetRowIndex: rowIndex, targetColoumIndex: columnIndex)  {
+                    moveOneColoumOfCardsFromTopToBottom(columnIndex: columnIndex)
+                }
+            }
         }
     }
-}
 
-// getNextColoumIndexのカードがselectedCardValueと一致する場合trueを返す。一致しない場合falseを返す。
-// getNextColoumIndexのカードが存在しない場合はnilを返す。
-//getTargetCard
-func isMatchCard(selectedCardValue: String,
-                 rowIndex: Int,
-                 coloumIndex: Int,
-                 getTargetRowIndex: (Int) -> Int,
-                 getTargetColoumIndex: (Int) -> Int) -> Bool? {
-
-    let targetColoumIndex = getTargetColoumIndex(coloumIndex)
-    let targetRowIndex = getTargetRowIndex(rowIndex)
-
-    // カードインデックスが存在するか確認
-    guard cardsState.existsIndex(targetRowIndex: targetRowIndex, targetColoumIndex: targetColoumIndex) else {
-        return nil
+    // 上のカードを下に移動する
+    func moveCardsFromTopToBottom(rowIndex: Int, columnIndex: Int) -> Bool {
+        var targetRowIndex = rowIndex
+        var isMoved = false
+        while existsIndex(targetRowIndex: targetRowIndex, targetColoumIndex: columnIndex) {
+            if existsIndex(targetRowIndex: targetRowIndex - 1, targetColoumIndex: columnIndex) {
+                cards[targetRowIndex][columnIndex].value = cards[targetRowIndex - 1][columnIndex].value
+                isMoved = true
+            } else {
+                cards[targetRowIndex][columnIndex].value = ""
+            }
+            targetRowIndex = targetRowIndex-1
+        }
+        return isMoved
     }
-    let targetCardValue = cardsState.cards[rowIndex][targetColoumIndex].value
-    return selectedCardValue == targetCardValue
+
+
+    // targetRowIndexより上にカードが存在するか
+    func existsTargetCard(targetRowIndex: Int, targetColoumIndex: Int) -> Bool {
+        for rowIndex in (0..<targetRowIndex) {
+            if existsIndex(targetRowIndex: rowIndex, targetColoumIndex: targetColoumIndex)
+                && cards[rowIndex][targetColoumIndex].value != "" {
+                return true
+            }
+        }
+        return false
+    }
+
+    func isBlankColumn(columnIndex: Int) -> Bool {
+        for rowIndex in 0..<Game.numberOfRows {
+            if cards[rowIndex][columnIndex].value != "" {
+                return false
+            }
+        }
+        return true
+    }
+
+    func moveCardsFromRightToLeft() {
+        for columnIndex in 0..<Game.numberOfColumns {
+            // １列ブランクかつ右の列に値が存在する場合
+            if isBlankColumn(columnIndex: columnIndex) &&
+                existsIndex(targetRowIndex: 0, targetColoumIndex: columnIndex + 1) &&
+                !isBlankColumn(columnIndex: columnIndex + 1) {
+                moveOneColumnOfCardsFromRightToLeft(columnIndex: columnIndex)
+            }
+        }
+
+    }
+
+    // 指定した空の1列から全体を左に移動する
+    func moveOneColumnOfCardsFromRightToLeft(columnIndex: Int) {
+        for rowIndex in 0..<Game.numberOfRows {
+            var currentColumnIndex = columnIndex
+            while true {
+                if existsIndex(targetRowIndex: rowIndex, targetColoumIndex: currentColumnIndex + 1) {
+                    cards[rowIndex][currentColumnIndex].value = cards[rowIndex][currentColumnIndex + 1].value
+                    currentColumnIndex += 1
+                } else {
+                    cards[rowIndex][currentColumnIndex].value = ""
+                    break
+                }
+
+            }
+        }
+    }
+
 }
 
-
-
-func updateScore() {
-    score.value += 1
-}
-
-struct Game {
-    var score: Score
-    var cards: CardsState
-//    var gameState: GameState
-
-}
-func startNewGame() {
-    cardsState.reset()
-    cardsState.printCards()
-}
-
-
-
-
-var cardsState = CardsState()
 var score = Score(totalValue: 0, value: 0)
-var game = Game(score: score, cards: cardsState)
+var game = Game()
+game.startNewGame()
 
-startNewGame()
-
-//tapedCard(rowIndex: 0, coloumIndex: 0)
-//tapedCard(rowIndex: 0, coloumIndex: 1)
-tapedCard(rowIndex: 2, coloumIndex: 2)
-
+game.tapedCard(rowIndex: 2, coloumIndex: 2)
 
 
 
--- samegame2.playground/Contents.swift
+++ samegame2.playground/Contents.swift
@@ -1,26 +1,41 @@
 import UIKit
 
-struct Card<CardContent> {
-    var value: CardContent
-    var isFixed: Bool = false
+struct Card {
+    var value: String
+    var isFixed: Bool
+
+
+
 }
 
 class Game {
 
-    static let numberOfRows =  4
-    static let numberOfColumns = 5
+    static let numberOfRows =  3
+    static let numberOfColumns = 3
     static let cardValues = ["👻", "🐤"]
 
-    var cards = [Card<String>]()
+    var cards = [Card]()
 
     func resetCards() {
-        for _ in 0..<Game.numberOfRows {
-            for _ in 0..<Game.numberOfColumns {
-                let value = Game.cardValues.randomElement()!
-                let card = Card(value: value)
-                cards.append(card)
-            }
-        }
+//        for _ in 0..<Game.numberOfRows {
+//            for _ in 0..<Game.numberOfColumns {
+//                let value = Game.cardValues.randomElement()!
+//                let card = Card(value: value)
+//                cards.append(card)
+//            }
+//        }
+
+        let 👻 = Card(value: "👻", isFixed: false)
+        let 🐤 = Card(value: "🐤", isFixed: false)
+
+        cards = [👻, 🐤, 🐤,
+                 👻, 👻, 👻,
+                 👻, 🐤, 👻]
+
+//        cards = [Card(value: "0"), Card(value: "1"), Card(value: "2"),
+//                 Card(value: "3"), Card(value: "4"), Card(value: "5"),
+//                 Card(value: "6"), Card(value: "7"), Card(value: "8")]
+
     }
     func printCards() {
         for rowIndex in 0..<Game.numberOfRows {
@@ -28,7 +43,7 @@ class Game {
             for columnsIndex in 0..<Game.numberOfColumns {
                 let index = rowIndex * Game.numberOfColumns + columnsIndex
 
-                if cards[index].value == "" {
+                if cards[index].isFixed {
                     cardRow += "[  ]"
                 } else {
                     cardRow += "[\(cards[index].value)]"
@@ -39,10 +54,157 @@ class Game {
         print("---------")
     }
 
+    func tapedCard(rowIndex: Int, columnsIndex: Int) {
+        let tappedIndex = getCardsIndex(rowIndex: rowIndex, columnsIndex: columnsIndex)
+        let tappedCardValue = cards[tappedIndex].value
+        deleteCards(tappedCardValue: tappedCardValue, tappedIndex: tappedIndex)
+        moveCards()
+    }
+
+    func moveCards() {
+        moveCardsFromTopToBottom()
+//        moveCardsFromRightToLeft()
+    }
+
+    func moveCardsFromTopToBottom() {
+        var arriveCards = [[Card]]()
+        for columnsIndex in 0..<Game.numberOfColumns {
+            var arriveCardsOneColumns = [Card]()
+            for i in columnsIndex..<cards.count where i % Game.numberOfColumns - columnsIndex == 0 {
+                if !cards[i].isFixed {
+                    arriveCardsOneColumns.append(cards[i])
+                }
+            }
+            arriveCards.append(arriveCardsOneColumns)
+        }
+        for columnsIndex in 0..<Game.numberOfColumns {
+            var arriveCardsOneColumns = arriveCards[columnsIndex]
+            for i in (columnsIndex..<cards.count).reversed() where i % Game.numberOfColumns - columnsIndex == 0 {
+                if let newCard = arriveCardsOneColumns.popLast() {
+                    cards[i] = newCard
+                } else {
+                    cards[i] = Card(value: "", isFixed: true)
+                }
+            }
+        }
+    }
+
+    func deleteCards(tappedCardValue: String, tappedIndex: Int) {
+        // タップされたカードの上下左右を消す
+        deleteCardsOnFourDirections(tappedCardValue: tappedCardValue, index: tappedIndex)
+    }
+
+    // 対象となるカードの上下左右にあるカードを消す
+    func deleteCardsOnFourDirections(tappedCardValue: String, index: Int) {
+
+        //上にあるカードを消す
+        var isFixedCardTop = false
+        if let card = getTopCard(index: index) {
+            if !card.isFixed && card.value == tappedCardValue {
+                cards[getTopCardIndex(index: index)].isFixed = true
+                isFixedCardTop = true
+            }
+        }
+        // 下にあるカードを消す
+        var isFixedCardBottom = false
+        if let card = getBottomCard(index: index) {
+            if !card.isFixed && card.value == tappedCardValue {
+                cards[getBottomCardIndex(index: index)].isFixed = true
+                isFixedCardBottom = true
+            }
+        }
+        // 左にあるカードを消す
+        var isFixedCardLeft = false
+        if let card = getLeftCard(index: index) {
+            if !card.isFixed && card.value == tappedCardValue {
+                cards[getLeftCardIndex(index: index)].isFixed = true
+                isFixedCardLeft = true
+            }
+        }
+        // 右にあるカードを消す
+        var isFixedCardRight = false
+        if let card = getRightCard(index: index) {
+            if !card.isFixed && card.value == tappedCardValue {
+                cards[getRightCardIndex(index: index)].isFixed = true
+                isFixedCardRight = true
+            }
+        }
+
+        // １つでも消したカードがあれば対象カードも消す
+        if isFixedCardTop || isFixedCardBottom || isFixedCardLeft || isFixedCardRight {
+            cards[index].isFixed = true
+        }
+
+        if isFixedCardTop && getTopCard(index: index) != nil {
+            // 対象を上に移動して再起呼び出し
+            deleteCardsOnFourDirections(tappedCardValue: tappedCardValue, index: getTopCardIndex(index: index))
+        }
+
+        if isFixedCardBottom && getBottomCard(index: index) != nil {
+            // 対象を下に移動して再起呼び出し
+            deleteCardsOnFourDirections(tappedCardValue: tappedCardValue, index: getBottomCardIndex(index: index))
+        }
+
+        if isFixedCardLeft && getLeftCard(index: index) != nil {
+            // 対象を左に移動して再起呼び出し
+            deleteCardsOnFourDirections(tappedCardValue: tappedCardValue, index: getLeftCardIndex(index: index))
+        }
+
+        if isFixedCardRight && getRightCard(index: index) != nil {
+            // 対象を右に移動して再起呼び出し
+            deleteCardsOnFourDirections(tappedCardValue: tappedCardValue, index: getRightCardIndex(index: index))
+        }
+    }
+
+    func getCardsIndex(rowIndex: Int, columnsIndex: Int) -> Int {
+        return rowIndex * Game.numberOfColumns + columnsIndex
+    }
+
+    func getLeftCard(index: Int) -> Card? {
+        if index % Game.numberOfColumns == 0 {
+            return nil
+        } else {
+            return cards[getLeftCardIndex(index: index)]
+        }
+    }
+    func getRightCard(index: Int) -> Card? {
+        if (index + 1) % Game.numberOfColumns == 0 {
+            return nil
+        } else {
+            return cards[getRightCardIndex(index: index)]
+        }
+    }
+    func getTopCard(index: Int) -> Card? {
+        if index < Game.numberOfColumns {
+            return nil
+        } else {
+            return cards[getTopCardIndex(index: index)]
+        }
+    }
+    func getBottomCard(index: Int) -> Card? {
+        if index + Game.numberOfColumns >= Game.numberOfColumns * Game.numberOfRows {
+            return nil
+        } else {
+            return cards[getBottomCardIndex(index: index)]
+        }
+    }
+    func getTopCardIndex(index: Int) -> Int {
+        index - Game.numberOfColumns
+    }
+    func getBottomCardIndex(index: Int) -> Int {
+        index + Game.numberOfColumns
+    }
+    func getLeftCardIndex(index: Int) -> Int {
+        index - 1
+    }
+    func getRightCardIndex(index: Int) -> Int {
+        index + 1
+    }
 }
 
 let game = Game()
 game.resetCards()
 game.printCards()
-
+game.tapedCard(rowIndex: 1, columnsIndex: 1)
+game.printCards()
 
